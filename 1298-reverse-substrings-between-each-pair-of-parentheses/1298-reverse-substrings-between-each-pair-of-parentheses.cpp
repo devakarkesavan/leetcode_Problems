8d13bb4d678aa12f08b4c65761c447a8e500d226
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-07-11 08:58:38 +0530

    Time: 0 ms (100%), Space: 8.2 MB (41.82%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,27 +1,27 @@
 class Solution {
 int i = 0;
 public:
-    string reverseParentheses(string s) {
-        return helper(s);
-    }
-
-    string helper(string& s) {
-        string result;
-
-        while (i < s.length()) {
-            if (s[i] == ')') {
+string fun(string& s) {
+        string result="";
+        while(i<s.length()){
+            if(s[i]==')'){
                 i++;
-                reverse(result.begin(), result.end());
+                reverse(result.begin(),result.end());
                 return result;
-            } else if (s[i] == '(') {
+            }
+            else if(s[i]=='('){
                 i++;
-                string st = helper(s);
-                result += st;
-            } else {
-                result += s[i];
+                string k = fun(s);
+                result+=k;
+            }
+            else{
+                result+=s[i];
                 i++;
             }
         }
         return result;
+}
+    string reverseParentheses(string s) {
+        return fun(s);
     }
 };
