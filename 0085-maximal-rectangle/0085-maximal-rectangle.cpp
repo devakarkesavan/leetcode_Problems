f026b0e90004f7f1ebba90aa6de1b1c966e75e5d
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-08-03 23:38:34 +0530

    Time: 46 ms (12.54%), Space: 16.7 MB (90.77%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
-    int findLargestAreaInHistogram(int n, vector<int>& rowHistogram) {
-        int area = 0;
-        for (int i = 0; i < n; i++) {
+    void fun(vector<int>his,int &maxi){
+        int n = his.size();
+        for(int i=0;i<n;i++){
             int breadth = 1;
-            // area = max(area, rowHistogram[i] * breadth);
-            for (int j = i - 1; j >= 0; j--) {
-                if (rowHistogram[j] >= rowHistogram[i]) {
+            for(int j=i-1;j>=0;j--){
+                if(his[j] >= his[i]){
                     breadth++;
-                } else {
+                }
+                else{
                     break;
                 }
             }
-            for (int j = i + 1; j < n; j++) {
-                if (rowHistogram[j] >= rowHistogram[i]) {
+            for(int j=i+1;j<n;j++){
+                if(his[j]>=his[i]){
                     breadth++;
-                } else {
+                }
+                else{
                     break;
                 }
             }
-            area = max(area, rowHistogram[i] * breadth);
+            maxi = max(maxi,his[i]*breadth);
         }
-        return area;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size();
+        int n  = matrix.size();
         int m = matrix[0].size();
-        int area = 0;
-        vector<int> rowHistogram(m, 0);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; ++j) {
-                if (matrix[i][j] == '1') {
-                    rowHistogram[j]++;
-                } else {
-                    rowHistogram[j] = 0;
+        vector<int>his(m,0);
+        int maxi = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    his[j]++;
+                }
+                else{
+                    his[j] = 0;
                 }
             }
-            area = max(area, findLargestAreaInHistogram(m, rowHistogram));
+            fun(his,maxi);
         }
-        return area;
+        return maxi;
     }
 };
