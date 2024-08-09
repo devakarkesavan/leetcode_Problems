9068db56870e620eb10b600653851e599c28739a
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-08-09 09:34:15 +0530

    Time: 6 ms (34.92%), Space: 9.7 MB (31.68%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int check(int ind,vector<int>&dp){
+        if(ind==0){
+            return 0;
+        }
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        return dp[ind] = ind%2 + check(ind/2,dp);
+    }
+    vector<int> countBits(int n) {
+        vector<int>dp(n+1,-1);
+        vector<int>ans(n+1);
+        for(int i=0;i<=n;i++){
+            ans[i] = check(i,dp);
+        }
+        return ans;
+
+    }
+};
