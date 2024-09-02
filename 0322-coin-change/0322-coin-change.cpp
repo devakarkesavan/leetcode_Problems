32c6030ec050f890dfbc9268c7879b62761e264b
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-09-02 22:37:11 +0530

    Time: 32 ms (63.19%), Space: 55.4 MB (7.9%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -22,25 +22,27 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        vector<int>prev(amount+1,0),cur(amount+1,0);
         for(int i=0;i<=amount;i++){
             if(i%coins[0]==0){
-                dp[0][i] = i/coins[0];
+                prev[i] = i/coins[0];
             }
             else{
-                dp[0][i] = 1e9;
+                prev[i] = 1e9;
             }
         }
         for(int ind=1;ind<n;ind++){
             for(int tar=0;tar<=amount;tar++){
-                int nottake = dp[ind-1][tar];
+                int nottake = prev[tar];
                 int take = 1e9;
                 if(coins[ind]<=tar){
-                    take = 1+dp[ind][tar-coins[ind]];
+                    take = 1+cur[tar-coins[ind]];
                 }
-                dp[ind][tar] = min(take,nottake);
+                cur[tar] = min(take,nottake);
             }
+            prev = cur;
         }
-        int k =  dp[n-1][amount];
+        int k =  prev[amount];
         if(k==1e9){
             return -1;
         }
