437f2c44c9a52d82c4a93e2c715d023616c250e6
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-10-09 14:58:08 +0530

    Time: 20 ms (73.85%), Space: 25.9 MB (69.6%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    int fun(int ind1,int ind2,string s,string t,vector<vector<int>>&dp){
-        if(ind1<0 | ind2<0){
+    int fun(int i,int j,string s,string t,vector<vector<int>>&dp){
+        if(i==0 || j==0){
             return 0;
         }
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        int val;
-        if(s[ind1]==t[ind2]){
-            val =  1+fun(ind1-1,ind2-1,s,t,dp);
+        if(s[i-1]==t[j-1]){
+            return dp[i][j] = 1+fun(i-1,j-1,s,t,dp);
         }
-        else{
-            val =  0 + max(fun(ind1-1,ind2,s,t,dp),fun(ind1,ind2-1,s,t,dp));
-        }
-        return dp[ind1][ind2] = val;
+        return dp[i][j] = max(fun(i-1,j,s,t,dp),fun(i,j-1,s,t,dp));
     }
     int longestCommonSubsequence(string s, string t) {
         int n = s.size();
-        int m =  t.size();
+        int m = t.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        // return fun(n-1,m-1,s,t,dp);
-        for(int ind1=1;ind1<=n;ind1++){
-            for(int ind2=1;ind2<=m;ind2++){
-                int val;
-                if(s[ind1-1]==t[ind2-1]){
-                    val =  1+dp[ind1-1][ind2-1];
+        // return fun(n,m,s,t,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
                 }
                 else{
-                    val =  0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
                 }
-                dp[ind1][ind2] = val;
             }
         }
         return dp[n][m];
