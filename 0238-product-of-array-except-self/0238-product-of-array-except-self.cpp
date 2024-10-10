6a25c665360fd6e77f0cfb3e632f5afd69e79cb3
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-10-10 22:30:32 +0530

    Time: 28 ms (50.99%), Space: 40.4 MB (28.79%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -6,7 +6,7 @@ public:
         vector<int>right(n,1);
         left[0] = 1;
         for(int i=1;i<n;i++){
-            left[i] = left[i-1] * nums[i-1];
+            left[i] = left[i-1]*nums[i-1];
         }
         right[n-1] = 1;
         for(int i=n-2;i>=0;i--){
