644177974206179b7287711abede5b0dc724bf9d
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-10-11 12:56:56 +0530

    Time: 0 ms (100%), Space: 7.8 MB (71.34%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int open = 0, mismatch = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(')
+        int open = 0,mismatch = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
                 open++;
-            else {
-                if (open > 0)
+            }
+            else{
+                if(open>0){
                     open--;
-                else
+                }
+                else{
                     mismatch++;
+                }
             }
         }
-        return open + mismatch;
+        return open+mismatch;
     }
 };
