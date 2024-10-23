4a3d60ea31e5995c294aec836f2298cc31b9c2c1
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-10-23 10:39:04 +0530

    Time: 0 ms (100%), Space: 16.6 MB (39.02%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int time = 0;
+        queue<pair<pair<int,int>,int>>q;
         int n = grid.size();
         int m = grid[0].size();
-
-        queue<pair<pair<int,int>,int>>q;
-        int cnt = 0;
         vector<vector<int>>vis(n,vector<int>(m,0));
+        int cnt = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
+                    vis[i][j] = 1;
                     q.push({{i,j},0});
-                    vis[i][j] = 2;
                 }
                 if(grid[i][j]==1){
                     cnt++;
                 }
             }
         }
-        int time = 0;
         int cnt1 = 0;
-        int dr[] = {0,-1,0,1};
-        int dc[] = {-1,0,1,0};
+        
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
+            time = max(t,time);
             q.pop();
-            time = max(time,t);
+
             for(int i=0;i<4;i++){
-                int nrow = dr[i]+r;
+                int nrow  = dr[i]+r;
                 int ncol = dc[i]+c;
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==1 && !vis[nrow][ncol]){
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol] = 2;
+
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1){
+                    vis[nrow][ncol] = 1;
                     cnt1++;
+                    q.push({{nrow,ncol},t+1});
                 }
             }
-            
         }
-        if(cnt!=cnt1){
+        if(cnt!=cnt1 ){
             return -1;
         }
         return time;
