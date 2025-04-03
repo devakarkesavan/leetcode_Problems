d2154c00a823ecc7088f4258f43015892f263fab
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2025-04-03 05:40:47 +0530

    Time: 0 ms (100%), Space: 26 MB (79.91%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,26 +2,11 @@ class Solution {
 public:
     int trap(vector<int>& arr) {
         int n = arr.size();
-        // vector<int>rightmax(n);
-        // vector<int>leftmax(n);
-        // leftmax[0] = height[0];
-        // for(int i=1;i<n;i++){
-        //     leftmax[i] = max(leftmax[i-1],height[i]);
-        // }
-        // rightmax[n-1] = height[n-1];
-        // for(int i=n-2;i>=0;i--){
-        //     rightmax[i] = max(rightmax[i+1],height[i]);
-        // }
-        // int sum = 0;
-        // for(int i=0;i<n;i++){
-        //     sum+= min(leftmax[i],rightmax[i]) - height[i];
-        // }
-        // return sum;
-        int left = 0,right = n-1,total = 0,leftmax = arr[left],rightmax = arr[n-1];
+        int left = 0,right = n-1,leftmax = arr[0],rightmax = arr[n-1],tot = 0;
         while(left<right){
             if(arr[left]<=arr[right]){
-                if(leftmax > arr[left]){
-                    total+=leftmax-arr[left];
+                if(arr[left]<=leftmax){
+                    tot+=leftmax-arr[left];
                 }
                 else{
                     leftmax = arr[left];
@@ -29,8 +14,8 @@ public:
                 left++;
             }
             else{
-                if(rightmax > arr[right]){
-                    total+=rightmax-arr[right];
+                if(arr[right]<=rightmax){
+                    tot+=rightmax-arr[right];
                 }
                 else{
                     rightmax = arr[right];
@@ -38,6 +23,6 @@ public:
                 right--;
             }
         }
-        return total;
+        return tot;
     }
 };
