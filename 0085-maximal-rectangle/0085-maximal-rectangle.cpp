1470e9c21018c519fd29f90b45f82d9dd39092dc
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2025-04-04 21:03:30 +0530

    Time: 6 ms (68.44%), Space: 16.2 MB (94%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
-    void fun(int &ans,vector<int>&his){
+
+    void fun(vector<int>&his,int &maxi){
         int n = his.size();
         for(int i=0;i<n;i++){
             int b = 1;
-            for(int j=i-1;j>=0;j--){
-                if(his[i]<=his[j]){
-                    b++;
-                }
-                else{
-                    break;
-                }
-            }
             for(int j=i+1;j<n;j++){
                 if(his[i]<=his[j]){
                     b++;
@@ -20,14 +13,22 @@ public:
                     break;
                 }
             }
-            ans = max(ans,his[i]*b);
+            for(int j=i-1;j>=0;j--){
+                if(his[i]<=his[j]){
+                    b++;
+                }
+                else{
+                    break;
+                }
+            }
+            maxi = max(maxi,his[i]*b);
         }
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int ans = 0;
         vector<int>his(m,0);
+        int maxi = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]=='1'){
@@ -37,8 +38,8 @@ public:
                     his[j] = 0;
                 }
             }
-            fun(ans,his);
+            fun(his,maxi);
         }
-        return ans;
+        return maxi;
     }
 };
